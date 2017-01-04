9efb381f60a0f669298d1293f729a3180531e4ee
Eliza <eliza.nitoi@gmail.com>
2017-01-04 22:37:50 +0200

    Added inputs for search flight and binding

--- frontend/src/app/search/search.component.ts
+++ frontend/src/app/search/search.component.ts
@@ -7,9 +7,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchComponent implements OnInit {
 
+  cities_departure: string[] = ['Cluj','Bucuresti', 'Timisoara'];
+  cities_destination: string[] = ['Botosani','Harghita', 'Constanta'];
+
+  city_departure: string = this.cities_departure[0];
+  city_destination: string = this.cities_destination[0];
+  flight_date: Date;
+
   constructor() { }
 
   ngOnInit() {
+
+  }
+
+  onChangeCityDeparture(newValue) {
+    console.log(newValue,"selected value");
+  }
+
+  onChangeCityDestination(newValue) {
+    console.log(newValue,"selected value");
+  }
+
+  onChangeDate(value) {
+    this.flight_date = value;
+    console.log(value);
+  }
+
+  searchFlight(): void {
+    console.log(this.city_departure, this.flight_date);
   }
 
 }
