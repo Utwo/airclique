cb084bd0644fc55787f0cf037879036a9131ada5
Unknown <stetcu.razvan@gmail.com>
2017-01-04 15:09:58 +0200

    Setup for login input

--- frontend/src/app/search/search.component.ts
+++ frontend/src/app/search/search.component.ts
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent implements OnInit {
+  sursa: string = '';
+  destinatie: string = '';
 
   constructor() { }
 
