79e31ebf2b43ea56f3e6d60cd3caa435e8283414
Eliza <eliza.nitoi@gmail.com>
2017-01-07 20:17:16 +0200

    Added rxjs-operator file and edited environments

---
+++ frontend/src/app/shared/state.service.ts
@@ -0,0 +1,34 @@
+import {Injectable} from '@angular/core';
+
+@Injectable()
+export class StateService {
+
+  constructor() {
+  }
+
+  storeState(token, user) {
+    localStorage.setItem('token', token);
+    localStorage.setItem('user', JSON.stringify(user));
+  }
+
+  getToken() {
+    return localStorage.getItem('token');
+  }
+
+  setUser(user) {
+    localStorage.setItem('user', JSON.stringify(user));
+  }
+
+  getUser() {
+    return JSON.parse(localStorage.getItem('user'));
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+    localStorage.removeItem('user');
+  }
+
+  isLoggedIn() {
+    return !!localStorage['token'];
+  }
+}
