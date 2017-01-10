c7f1702ea5f8003c2ad59d40e09c83cfa09dbd8b
Oana Muntean <oanamuntean8@gmail.com>
2017-01-10 15:17:02 +0200

    Get all cities request

--- backend/app/Http/routes.js
+++ backend/app/Http/routes.js
@@ -5,6 +5,8 @@ const Route = use('Route')
 Route.on('/').render('welcome')
 Route.post('/login', 'UserController.login')
 
+Route.get('/cities', 'CityController.index')
+
 Route.get('/flights', 'FlightController.index')
 Route.get('/flights/:id', 'FlightController.flight')
 
