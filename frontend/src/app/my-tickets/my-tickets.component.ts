d57bbaea31dabca8e0a5b2baa693b485ab05ea49
Razvan <stetcu.razvan@gmail.com>
2017-01-11 01:46:53 +0200

    Date changed from miliseconds to real date.

--- frontend/src/app/my-tickets/my-tickets.component.ts
+++ frontend/src/app/my-tickets/my-tickets.component.ts
@@ -1,27 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 import {MyTicketsService} from './my-tickets.service';
-import {StateService} from '../shared/state.service';
 import {IFlight} from '../shared/flight';
 
 @Component({
   selector: 'app-my-tickets',
   templateUrl: './my-tickets.component.html'
 })
+
 export class MyTicketsComponent implements OnInit {
   token: string;
+  flightsList: IFlight[];
   flights: IFlight[];
   errorMessage: string = null;
-
   constructor(private myTicketsService: MyTicketsService) {
-    this.myTickets();
   }
   ngOnInit() {
+    this.myTickets();
   }
 
+  processFlightsList() {
+    this.flightsList = this.flights.map(function(x){
+      x.departure_time = new Date(x.departure_time);
+      x.arrival_time = new Date(x.arrival_time);
+      return x;
+    });
+  }
   myTickets() {
     this.myTicketsService.myFlights()
         .subscribe(
-            data => this.flights = data,
+            data => {
+              this.flights = data
+              this.processFlightsList();
+            },
             err  => this.errorMessage = 'Error on receiving flights'
         );
   }
